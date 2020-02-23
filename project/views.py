e7d0c13fe53c2512f06ccf410fcc2b809d597bc3
pierkoo <pierkoo@gmail.com>
2020-02-23 01:25:53 +0100

    user autenthication added

--- project/views.py
+++ project/views.py
@@ -39,11 +39,15 @@ def login():
     if request.method == "POST":
         if request.form["username"] != app.config["USERNAME"] \
             or request.form["password"] != app.config["PASSWORD"]:
-        error = "Invalid credentials. Please try again."
-        return render_template("login.html", eroor=error)
-    else:
-        session["logged_in"] = True
-        flash('Welcome!')
-        return redirect(url_for("tasks"))
+            error = "Invalid credentials. Please try again."
+            return render_template("login.html", error=error)
+        else:
+            session["logged_in"] = True
+            flash('Welcome!')
+            return redirect(url_for("tasks"))
     return render_template("login.html")
 
+
+@app.route("/tasks/")
+def tasks():
+    return "BBB"
