59a996edf7142e65889bc42b6b189ee7366bde83
pierkoo <pierkoo@gmail.com>
2020-03-05 13:34:35 +0100

    custom error pages & error loging added

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,10 +16,13 @@ class MainTests(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         app.config["WTF_CSRF_ENABLED"] = False
+        app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+        self.assertEquals(app.debug, False)
 
     def tearDown(self):
         db.session.remove()
@@ -43,14 +46,18 @@ class MainTests(unittest.TestCase):
 
     def test_500_error(self):
         bad_user = User(
-            name = "Janek",
+            name = "Janek1",
             email= "janek@wp.pl",
             password = "janek1"
         )
         db.session.add(bad_user)
         db.session.commit()
-        response = self.login("Janek", "janek1")
-        self.assertEquals(response.status_code, 500)
+        self.assertRaises(ValueError, self.login, "Janek1", "janek1")
+        try:
+            response = self.login("Janek1", "janek1")
+            self.assertEquals(response.status_code, 500)
+        except ValueError:
+            pass
 
 if __name__ == "__main__":
     unittest.main()
